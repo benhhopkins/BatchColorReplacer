a6fc02ec35c68b0093ec84adcdfb57901b410a2f
castled <benhop88@gmail.com>
2015-11-29 15:25:42 -0700

    BatchColorReplacer Create

---
+++ BatchColorReplacer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace BatchColorReplacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colorDefs;
+            try
+            {
+               colorDefs = System.IO.File.ReadAllLines(args[0]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR>> Cannot read color definitions file, exception: " + e.ToString());
+                Console.WriteLine("USAGE: \nBatchColorReplacer colorDefinitions.txt image1.png image2.png ...");
+                return;
+            }
+
+            ProcessStartInfo converter = new ProcessStartInfo();
+            converter.FileName = @"C:\Packages\ImageMagick\convert.exe";
+            converter.UseShellExecute = false;
+            converter.RedirectStandardOutput = true;
+            for(int i = 1; i < args.Length; ++i)
+            {
+                try
+                {
+                    string[] files = { args[i] };
+                    if (args[i].Contains('*'))
+                    {
+                        files = Directory.GetFiles(@".\", args[i]);
+                    }
+
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine("Processing " + file);
+                        foreach (string colorReplace in colorDefs)
+                        {
+                            Process p = new Process();
+
+                            converter.Arguments = file + " -fill " + colorReplace.Split(' ')[1] + " -opaque " + colorReplace.Split(' ')[0] + " " + file;
+                            Console.WriteLine("    convert.exe " + converter.Arguments);
+
+                            p.StartInfo = converter;
+                            p.Start();
+                            Console.WriteLine(p.StandardOutput.ReadToEnd());
+                            p.WaitForExit();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ERROR>> " + e.ToString());
+                }
+            }
+        }
+    }
+}
